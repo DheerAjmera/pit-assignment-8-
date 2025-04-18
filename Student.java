7f4901196dcc93c513c8fe7a2cc533af063ae4c3
Dheer Ajmera <165947269+DheerAjmera@users.noreply.github.com>
2025-04-18 19:54:49 +0530

    Getter and setter initialization

--- Student.java
+++ Student.java
@@ -17,3 +17,17 @@ public class Student {
         this.batch = batch;
         this.cgpa = cgpa;
     }
+    
+    // Getters (accessor methods)
+    public String getName() { return name; }
+    public int getPRN() { return prn; }
+    public String getBranch() { return branch; }
+    public String getBatch() { return batch; }
+    public float getCGPA() { return cgpa; }
+
+    // Setters (mutator methods)
+    public void setName(String name) { this.name = name; }
+    public void setBranch(String branch) { this.branch = branch; }
+    public void setBatch(String batch) { this.batch = batch; }
+    public void setCGPA(float cgpa) { this.cgpa = cgpa; }
+
