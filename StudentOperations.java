a825575e6fb911835a7235b837b1a6b3b1143408
Dheer Ajmera <165947269+DheerAjmera@users.noreply.github.com>
2025-04-18 19:56:41 +0530

    Create StudentOperations.java
    initializ constructor

---
+++ StudentOperations.java
@@ -0,0 +1,11 @@
+// StudentOperations.java
+
+import java.util.*; // Importing the utility package for using ArrayList and other utilities
+
+public class StudentOperations {
+    private ArrayList<Student> students; // List to store Student objects
+
+    // Constructor initializes the ArrayList
+    public StudentOperations() {
+        this.students = new ArrayList<>();
+    }
