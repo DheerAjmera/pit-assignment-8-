8e7a0412d858e767ba24f2b25c860c741cee1bab
Dheer Ajmera <165947269+DheerAjmera@users.noreply.github.com>
2025-04-18 19:57:31 +0530

    Update StudentOperations.java
    Method to add a new student to the list

--- StudentOperations.java
+++ StudentOperations.java
@@ -9,3 +9,14 @@ public class StudentOperations {
     public StudentOperations() {
         this.students = new ArrayList<>();
     }
+    // Method to add a new student to the list
+    public void addStudent(Student student) throws StudentAlreadyExistsException {
+        // Check for duplicate PRN
+        for (Student s : students) {
+            if (s.getPRN() == student.getPRN()) {
+                throw new StudentAlreadyExistsException("Student with PRN " + student.getPRN() + " already exists.");
+            }
+        }
+        // If no duplicate, add student to the list
+        students.add(student);
+    }
